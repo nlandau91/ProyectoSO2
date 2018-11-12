3b6ef8044553db1914f9815447344c33b1b1526c
nlandau91 <43214457+nlandau91@users.noreply.github.com>
2018-11-11 22:47:01 -0300

    Add files via upload

--- ProyectoSO2.1.1.ii/main.c
+++ ProyectoSO2.1.1.ii/main.c
@@ -13,13 +13,14 @@ pid_t p[5];
 char *sg = "1";
 
 void fun_A(){
+    //cierro los pipes que no voy a usar
     close(pipe_abc[1]);
     close(pipe_d[0]);
     close(pipe_e[0]);
     close(pipe_e[1]);
     while(1){
         char c[2];
-        read(pipe_abc[0],c,2);
+        read(pipe_abc[0],c,2);//lectura bloqueante, hasta que recibo algo en el pipe, no avanzo
         printf("A");
         fflush(stdout);
         write(pipe_d[1],sg,2);
@@ -27,13 +28,14 @@ void fun_A(){
 }
 
 void fun_B(){
+    //cierro los pipes que no voy a usar
     close(pipe_abc[1]);
     close(pipe_d[0]);
     close(pipe_e[0]);
     close(pipe_e[1]);
     while(1){
         char c[2];
-        read(pipe_abc[0],c,2);
+        read(pipe_abc[0],c,2);//lectura bloqueante, hasta que recibo algo en el pipe, no avanzo
         printf("B");
         fflush(stdout);
         write(pipe_d[1],sg,2);
@@ -41,13 +43,14 @@ void fun_B(){
 }
 
 void fun_C(){
+    //cierro los pipes que no voy a usar
     close(pipe_abc[1]);
     close(pipe_d[0]);
     close(pipe_e[0]);
     close(pipe_e[1]);
     while(1){
         char c[2];
-        read(pipe_abc[0],c,2);
+        read(pipe_abc[0],c,2);//lectura bloqueante, hasta que recibo algo en el pipe, no avanzo
         printf("C");
         fflush(stdout);
         write(pipe_d[1],sg,2);
@@ -55,14 +58,15 @@ void fun_C(){
 }
 
 void fun_D(){
+    //cierro los pipes que no voy a usar
     close(pipe_abc[0]);
     close(pipe_abc[1]);
     close(pipe_d[1]);
     close(pipe_e[0]);
     while(1){
         char c[2];
-        read(pipe_d[0],c,2);
-        read(pipe_d[0],c,2);
+        read(pipe_d[0],c,2);//lectura bloqueante, hasta que recibo algo en el pipe, no avanzo
+        read(pipe_d[0],c,2);//necesitos dos habilitaciones de (a/b/c)
         printf("D");
         fflush(stdout);
         write(pipe_e[1],sg,2);
@@ -70,17 +74,18 @@ void fun_D(){
 }
 
 void fun_E(){
+    //cierro los pipes que no voy a usar
     close(pipe_abc[0]);
     close(pipe_d[0]);
     close(pipe_d[1]);
     close(pipe_e[1]);
     while(1){
         char c[2];
-        read(pipe_e[0],c,2);
-        printf("E");
+        read(pipe_e[0],c,2);//lectura bloqueante, hasta que recibo algo en el pipe, no avanzo
+        printf("E\n");//el salto de linea es para facilitar la lectura, la secuencia deberia ser de corrido
         fflush(stdout);
         sleep(1);
-        write(pipe_abc[1],sg,2);
+        write(pipe_abc[1],sg,2);//habilito dos veces a los caracteres (a/b/c)
         write(pipe_abc[1],sg,2);
     }
 }
@@ -131,21 +136,22 @@ void crearProcesos(){
 
 int main()
 {
+    printf("La secuencia es (A o B o C)(A o B o C)DE...\n");
     crearPipes();
     crearProcesos();
+    //cierro los pipes que no voy a usar
     close(pipe_abc[0]);
     close(pipe_d[0]);
     close(pipe_d[1]);
     close(pipe_e[0]);
     close(pipe_e[1]);
-    write(pipe_abc[1],sg,2);
+    write(pipe_abc[1],sg,2);//estos writes comienzan la secuencia, luego cierro los pipes
     write(pipe_abc[1],sg,2);
     close(pipe_abc[1]);
-    while(wait(NULL)){
+    while(wait(NULL)){//espero a que terminen los procesos
         if (errno == ECHILD){
             break;
         }
     }
-
     return 0;
 }
