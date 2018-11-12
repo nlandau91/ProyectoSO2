2eb1ddcbabda2e6ef8084584458219c53f538bda
nlandau91 <43214457+nlandau91@users.noreply.github.com>
2018-11-11 22:46:43 -0300

    Add files via upload

--- ProyectoSO2.1.1.iii/main.c
+++ ProyectoSO2.1.1.iii/main.c
@@ -11,11 +11,12 @@ int pipe_b[2];
 int pipe_c[2];
 int pipe_d[2];
 int pipe_e[2];
-int pipe_aux[2];
+int pipe_ab[2];
 pid_t p[5];
 char *sg = "1";
 
 void fun_A(){
+    //cierro los pipes que no voy a usar
     close(pipe_a[1]);
     close(pipe_b[0]);
     close(pipe_c[0]);
@@ -23,24 +24,26 @@ void fun_A(){
     close(pipe_d[1]);
     close(pipe_e[0]);
     close(pipe_e[1]);
-    close(pipe_aux[1]);
+    close(pipe_ab[1]);
     while(1){
+        sleep(1);
         char c[2];
-        read(pipe_a[0],c,2);
-        read(pipe_aux[0],c,2);
+        read(pipe_a[0],c,2);//me habilita B
+        read(pipe_ab[0],c,2);
         printf("A");
         fflush(stdout);
-        write(pipe_c[1],sg,2);
-        write(pipe_b[1],sg,2);
-        read(pipe_a[0],c,2);
-        read(pipe_aux[0],c,2);
+        write(pipe_c[1],sg,2);//habilito a C inmediatamente
+        write(pipe_b[1],sg,2);//la proxima no es una A, es una B
+        read(pipe_a[0],c,2);//me habilita B
+        read(pipe_ab[0],c,2);
         printf("A");
         fflush(stdout);
-        write(pipe_b[1],sg,2);
+        write(pipe_b[1],sg,2);//habilito a B
     }
 }
 
 void fun_B(){
+    //cierro los pipes que no voy a usar
     close(pipe_a[0]);
     close(pipe_b[1]);
     close(pipe_c[0]);
@@ -48,25 +51,25 @@ void fun_B(){
     close(pipe_d[1]);
     close(pipe_e[0]);
     close(pipe_e[1]);
-
-    close(pipe_aux[1]);
+    close(pipe_ab[1]);
     while(1){
         char c[2];
-        read(pipe_b[0],c,2);
-        read(pipe_aux[0],c,2);
+        read(pipe_b[0],c,2);//me habilita A
+        read(pipe_ab[0],c,2);
         printf("B");
         fflush(stdout);
-        write(pipe_c[1],sg,2);
-        write(pipe_a[1],sg,2);
+        write(pipe_c[1],sg,2);//habilito a C inmediatamente
+        write(pipe_a[1],sg,2);//la proxima no es una B, es una A
         read(pipe_b[0],c,2);
         printf("B");
         fflush(stdout);
-        write(pipe_c[1],sg,2);
-        write(pipe_a[1],sg,2);
+        write(pipe_c[1],sg,2);//habilito a C inmediatamente
+        write(pipe_a[1],sg,2);//cuando termine esta secuencia empieza por A denuevo
     }
 }
 
 void fun_C(){
+    //cierro los pipes que no voy a usar
     close(pipe_a[0]);
     close(pipe_a[1]);
     close(pipe_b[0]);
@@ -75,11 +78,11 @@ void fun_C(){
     close(pipe_d[0]);
     close(pipe_e[0]);
     close(pipe_e[1]);
-    close(pipe_aux[0]);
-    close(pipe_aux[1]);
+    close(pipe_ab[0]);
+    close(pipe_ab[1]);
     while(1){
         char c[2];
-        read(pipe_c[0],c,2);
+        read(pipe_c[0],c,2);//me habilita tanto A como B
         printf("C");
         fflush(stdout);
         write(pipe_d[1],sg,2);
@@ -87,6 +90,7 @@ void fun_C(){
 }
 
 void fun_D(){
+    //cierro los pipes que no voy a usar
     close(pipe_a[0]);
     close(pipe_a[1]);
     close(pipe_b[0]);
@@ -95,8 +99,8 @@ void fun_D(){
     close(pipe_c[1]);
     close(pipe_d[1]);
     close(pipe_e[0]);
-    close(pipe_aux[0]);
-    close(pipe_aux[1]);
+    close(pipe_ab[0]);
+    close(pipe_ab[1]);
     while(1){
         char c[2];
         read(pipe_d[0],c,2);
@@ -107,6 +111,7 @@ void fun_D(){
 }
 
 void fun_E(){
+    //cierro los pipes que no voy a usar
     close(pipe_a[0]);
     close(pipe_a[1]);
     close(pipe_b[0]);
@@ -116,14 +121,13 @@ void fun_E(){
     close(pipe_d[0]);
     close(pipe_d[1]);
     close(pipe_e[1]);
-    close(pipe_aux[0]);
+    close(pipe_ab[0]);
     while(1){
         char c[2];
         read(pipe_e[0],c,2);
-        printf("E");
+        printf("E\n");//el salto de linea es para facilitar la lectura, la secuencia deberia ser de corrido
         fflush(stdout);
-        sleep(1);
-        write(pipe_aux[1],sg,2);
+        write(pipe_ab[1],sg,2);//habilito a A o B, el que toque
     }
 }
 
@@ -149,7 +153,7 @@ void crearPipes(){
         perror("Error pipe");
         exit(EXIT_FAILURE);
     }
-    if(pipe(pipe_aux)==-1){
+    if(pipe(pipe_ab)==-1){
         perror("Error pipe");
         exit(EXIT_FAILURE);
     }
@@ -185,8 +189,11 @@ void crearProcesos(){
 
 int main()
 {
+    printf("La secuencia es ACDEBCDEABCDE...\n");
+
     crearPipes();
     crearProcesos();
+    //cierro los pipes que no voy a usar
     close(pipe_a[0]);
     close(pipe_b[0]);
     close(pipe_b[1]);
@@ -196,11 +203,11 @@ int main()
     close(pipe_d[1]);
     close(pipe_e[0]);
     close(pipe_e[1]);
-    close(pipe_aux[0]);
-    write(pipe_a[1],sg,2);
-    write(pipe_aux[1],sg,2);
+    close(pipe_ab[0]);
+    write(pipe_a[1],sg,2);//estos writes comienzan la secuencia, luego cierro los pipes
+    write(pipe_ab[1],sg,2);
     close(pipe_a[1]);
-    close(pipe_aux[1]);
+    close(pipe_ab[1]);
     while(wait(NULL)){//espero a que terminen los procesos
         if (errno == ECHILD){
             break;
